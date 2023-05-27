18112ebb46c8dd7323ac2b02073c5a587bec2f6b
tanvipatel99251 <134581002+tanvipatel99251@users.noreply.github.com>
2023-05-27 15:00:37 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-const express = require ("express")
+const express = require("express")
 const app = express()
-const PORT = 5000
+const PORT = 4000
 const path = require ("path")
 
-
-app.get("/",(req,resp)=>{
-    // resp.send("this is index page")
-    resp.sendFile(path.join(__dirname,"index.html"))
-})
-
-
-app.get("/home",(req,resp)=>{
-    resp.sendFile(path.join(__dirname,"home.html"))
-})
-
-app.get("*",(req,resp)=>{
-    resp.send("404 : page not found")
+app.get("/get",(req,resp)=>{
+    resp.sendFile(path.join(__dirname,"index.html"));
 })
 
 
 
 app.listen(PORT,()=>{
-    console.log("server running on port:"+PORT);
+  console.log("server running on port:" +PORT);
 })
